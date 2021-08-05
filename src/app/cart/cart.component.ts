5269471fbb13a6011a3a60f6700c3a57f243787b
lucagavioli <luca.gavioli@bs-company.com>
2021-08-05 16:53:46 +0200

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {CartService} from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -8,13 +9,24 @@ import {CartService} from '../cart.service';
 })
 export class CartComponent {
   items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
   // empty the Cart
   emptyCart() {
-    this.items= this.cartService.clearCart();
+    this.items = this.cartService.clearCart();
     window.alert('Cart successfully emptied');
   }
-  constructor(
-    private cartService: CartService
-  ) { }
-
+  //
+  onSubmit(): void {
+    // process checkout
+    this.emptyCart();
+    console.warn('Your order has been submitted!', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
